6dc0bc5db8bd5eac5866c24a650fb9e30e463d63
BaDu-HSN <bvanduijnhoven@solcon.nl>
2017-12-02 09:25:54 +0100

    issue #3

--- webkaart/balloon4.php
+++ webkaart/balloon4.php
@@ -306,7 +306,8 @@ function DisplayValues ($type, $tablerow, $disprow) {
 	$retval='';
 	for ($x = 1; $x < count($disprow)-1; $x++) {
 		if ($disprow[$x]['Type']=='Small') {
-			$retval .= '<div class="balloon-gegevensrij"><div class="balloon-gegtitel">'.vertaal($disprow[$x]['DisplayNaam']).'</div><div class="balloon-gegwaarde">'.utf8_encode($tablerow[$disprow[$x]['Column']]).' '.$disprow[$x]['Suffix'].'</div></div>';
+//			$retval .= '<div class="balloon-gegevensrij"><div class="balloon-gegtitel">'.vertaal($disprow[$x]['DisplayNaam']).'</div><div class="balloon-gegwaarde">'.utf8_encode($tablerow[$disprow[$x]['Column']]).' '.$disprow[$x]['Suffix'].'</div></div>';
+			$retval .= '<div class="balloon-gegevensrij"><div class="balloon-gegtitel">'.vertaal($disprow[$x]['DisplayNaam']).'</div><div class="balloon-gegwaarde">'.$tablerow[$disprow[$x]['Column']].' '.$disprow[$x]['Suffix'].'</div></div>';
 		} elseif ($disprow[$x]['Type']=='Big') {
 			$retval .= '<div class="balloon-gegevensbig"><div class="balloon-bigtitel">'.vertaal($disprow[$x]['DisplayNaam']).'</div><div class="balloon-bigwaarde">'.$tablerow[$disprow[$x]['Column']].'</div></div>';
 		} elseif ($disprow[$x]['Type']=='Func') {
